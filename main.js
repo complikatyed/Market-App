e2a3e7510db16014774fe43e57951bb21865ce30
Luke Lancaster <luketlancaster@gmail.com>
2015-02-02 14:16:09 -0600

    create rows with AJAX data

--- main.js
+++ main.js
@@ -1,59 +1,42 @@
-
-var baseUrl = 'http://dev.markitondemand.com/Api/v2/Quote/json?symbol=',
+'use strict';
+var baseUrl = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=',
     $symbol,
     newUrl;
 
 $('#buy').on('click', function() {
   $symbol = $('#symbol').val();
+  $symbol += '&callback=?';
   newUrl = baseUrl + $symbol;
-  $quantity = $('#quantity').val();
+  //$quantity = $('#quantity').val();
 
   runQuoteAndPrint();
 });
 
 function runQuoteAndPrint() {
-  $.get(newUrl, function(res) {
-    var symbol = res.Symbol;
-    var price = res.LastPrice;
-    var change = res.Change;
-    var percentChange = res.ChangePercent;
-    console.log(price, symbol, change, percentChange);
+  $.getJSON(newUrl, function(res) {
+    createTableData(res);
   });
 }
 
-var $tdSymbol = $('<td>' + symbol + '</td>');
-var $tdPrice = $('<td>' + price + '</td>');
-var $tdChange = $('<td>' + change + '</td>');
-var $tdChangePercent = $('<td>' + changePercent + '</td>');
+function createTableData(res) {
+  var name = res.Name;
+  var price = res.LastPrice;
+  var change = '$ ' + (res.Change).toFixed(2);
+  var percentChange = Math.round((res.ChangePercent) * 100) + ' %';
+
+  var $tr = $('<tr></tr>');
+  var $tdName = $('<td>' + name + '</td>');
+  $tr.append($tdName);
+  var $tdPrice = $('<td>' + price + '</td>');
+  $tr.append($tdPrice);
+  var $tdChange = $('<td>' + change + '</td>');
+  $tr.append($tdChange);
+  var $tdChangePercent = $('<td>' + percentChange + '</td>');
+  $tr.append($tdChangePercent);
+  $('#table').append($tr);
+}
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// grab the symbol
-
-// append to URL
-
-// on buy click, run AJAX call (to get data)
-
-// append table elements
-
-// include remove button
-
-// add functionality to remove button
-
-// total value = sum of price * quantity
