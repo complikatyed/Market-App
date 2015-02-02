b1d9a553a90eb7616567796e87cb4ec91e561623
Luke Lancaster <luketlancaster@gmail.com>
2015-02-02 13:18:24 -0600

    add function to get and run AJAX call and grab data from call

--- main.js
+++ main.js
@@ -1,7 +1,55 @@
+
+var baseUrl = 'http://dev.markitondemand.com/Api/v2/Quote/json?symbol=',
+    $symbol,
+    newUrl;
+
+$('#buy').on('click', function() {
+  $symbol = $('#symbol').val();
+  newUrl = baseUrl + $symbol;
+  $quantity = $('#quantity').val();
+
+  runQuoteAndPrint();
+});
+
+function runQuoteAndPrint() {
+  $.get(newUrl, function(res) {
+    var symbol = res.Symbol;
+    var price = res.LastPrice;
+    var change = res.Change;
+    var percentChange = res.ChangePercent;
+    console.log(price, symbol, change, percentChange);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // grab the symbol
+
 // append to URL
+
 // on buy click, run AJAX call (to get data)
+
 // append table elements
+
 // include remove button
+
 // add functionality to remove button
+
 // total value = sum of price * quantity
