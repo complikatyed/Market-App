f46bb933ab80d0d0d00e31bcb800bad087683a08
Katye Russell <katye.russell@gmail.com>
2015-02-02 12:31:29 -0600

    Adding comments with JS/jQuery plans.

---
+++ main.js
@@ -0,0 +1,7 @@
+// grab the symbol
+// append to URL
+// on buy click, run AJAX call (to get data)
+// append table elements
+// include remove button
+// add functionality to remove button
+// total value = sum of price * quantity
