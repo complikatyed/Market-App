5281783688bae5365e529557031c7f66d3230dd5
Katye Russell <katye.russell@gmail.com>
2015-02-03 14:09:47 -0600

    Adding remove button subtraction functionality and restructuring creation of stock objects.

--- main.js
+++ main.js
@@ -1,74 +1,63 @@
 var baseUrl = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=',
-  $symbol,
-  newUrl,
-  $quantity;
+    $symbol,
+    newUrl,
+    total = 0;
 
 $('#buy').on('click', function() {
   event.preventDefault();
   $symbol = $('#symbol').val();
   $symbol += '&callback=?';
   newUrl = baseUrl + $symbol;
-  $quantity = $('#quantity').val();
   grabQuoteData();
 });
 
-function grabQuoteData() {
-  $.getJSON(newUrl, function(res) {
-  getData(res);
+$('#table').on('click', '.remove', function() {
+  var $tr = $(this).closest("tr");
+  var qty = $tr.children('.quantity').text() * 1;
+  var price = $tr.children('.price').text() * 1;
+
+  total -= price * qty;
+  total = ((total * 100) / 100).toFixed(2);
+  $("p").text("Total: $" + total);
+
+  $tr.remove();
 });
+
+function grabQuoteData() {
+  $.getJSON(newUrl, getData);
 }
 
-var idCounter = 1;
-
 function getData(res) {
-  var name = res.Name;
-  var price = res.LastPrice;
-  var dayChange = '$ ' + (res.Change).toFixed(2);
-  var percentChange = Math.round((res.ChangePercent) * 100) + ' %';
+  res.quantity = parseInt($('#quantity').val());
+  var stock = new Stock(res);
 
-  var stock;
-  this.stocks = [];
-  stock = new Stock();
-  this.stocks.push(stock);
   createTableData(stock);
 
-  function Stock() {
-    this.companyName = name;
-    this.purchPrice = price;
-    this.quantity = $quantity;
-    this.dayChange = dayChange;
-    this.percentChange = percentChange;
-    this.id = idCounter++;
-  }
-
-  function createTableData(){
+  function createTableData(stock){
     var $tr = $('<tr>"stock.id"</tr>');
     var $tdName = $('<td>' + stock.companyName + '</td>');
     $tr.append($tdName);
-    var $tdPrice = $('<td class="price">' + stock.purchPrice + '</td>');
+    var $tdPrice = $('<td class="price">' + ((stock.purchPrice * 100) / 100).toFixed(2) + '</td>');
     $tr.append($tdPrice);
-    var $tdQuantity = $('<td>' + stock.quantity + '</td>');
+    var $tdQuantity = $('<td class="quantity">' + stock.quantity + '</td>');
     $tr.append($tdQuantity);
-    var $tdChange = $('<td>' + stock.dayChange + '</td>');
+    var $tdChange = $('<td>' + ((stock.dayChange * 100) / 100).toFixed(2) + '</td>');
     $tr.append($tdChange);
-    var $tdChangePercent = $('<td>' + stock.percentChange + '</td>');
+    var $tdChangePercent = $('<td>' + stock.percentChange.toFixed(2) + '%' + '</td>');
     $tr.append($tdChangePercent);
     var $tdButton = $('<button class="remove">Remove</button>');
     $tr.append($tdButton);
-    $('#table').on('click', '.remove', function() {
-      $(this).closest("tr").remove();
-    })
     $('#table').append($tr);
 
-    var total = 0;
-    $(".price").each(function() {
-      var value = $(this).text();
-      if(!isNaN(value) && value.length != 0) {
-      total += parseFloat(value);
-      }
+    total += parseFloat(stock.quantity) * parseFloat(stock.purchPrice);
     $("p").text("Total: $" + total.toFixed(2));
-    });
-
-// Need event listener here to recalcuate the total if rows are removed...
   }
+}
+
+function Stock(obj) {
+  this.companyName = obj.Name;
+  this.purchPrice = obj.LastPrice;
+  this.quantity = obj.quantity;
+  this.dayChange = obj.Change;
+  this.percentChange = obj.ChangePercent;
 }
