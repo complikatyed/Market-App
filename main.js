afe4200c54813942df3bdd757962eeb5ba657daf
Katye Russell <katye.russell@gmail.com>
2015-02-03 00:28:36 -0600

    Adding stocks as individual objects and total of price amounts (should have committed several times before now).

--- main.js
+++ main.js
@@ -1,49 +1,73 @@
-(function () {
+var baseUrl = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=',
+  $symbol,
+  newUrl,
+  $quantity;
 
-  'use strict';
-  var baseUrl = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=',
-      $symbol,
-      newUrl,
-      $quantity;
+$('#buy').on('click', function() {
+$symbol = $('#symbol').val();
+$symbol += '&callback=?';
+newUrl = baseUrl + $symbol;
+$quantity = $('#quantity').val();
+grabQuoteData();
+});
 
-  $('#buy').on('click', function() {
-    $symbol = $('#symbol').val();
-    $symbol += '&callback=?';
-    newUrl = baseUrl + $symbol;
-    $quantity = $('#quantity').val();
-    runQuoteAndPrint();
-  });
+function grabQuoteData() {
+	$.getJSON(newUrl, function(res) {
+	getData(res);
+});
+}
 
-  function runQuoteAndPrint() {
-    $.getJSON(newUrl, function(res) {
-      createTableData(res);
-    });
-  }
+var idCounter = 1;
 
-  function createTableData(res) {
-    var name = res.Name;
-    var price = res.LastPrice;
-    var change = '$ ' + (res.Change).toFixed(2);
-    var percentChange = Math.round((res.ChangePercent) * 100) + ' %';
+function getData(res) {
+  var name = res.Name;
+  var price = res.LastPrice;
+  var dayChange = '$ ' + (res.Change).toFixed(2);
+  var percentChange = Math.round((res.ChangePercent) * 100) + ' %';
 
-    var $tr = $('<tr></tr>');
-    var $tdName = $('<td>' + name + '</td>');
-    $tr.append($tdName);
-    var $tdPrice = $('<td>' + price + '</td>');
-    $tr.append($tdPrice);
-    var $tdQuantity = $('<td>' + $quantity + '</td>');
-    $tr.append($tdQuantity);
-    var $tdChange = $('<td>' + change + '</td>');
-    $tr.append($tdChange);
-    var $tdChangePercent = $('<td>' + percentChange + '</td>');
-    $tr.append($tdChangePercent);
-    var $tdButton = $('<button class="remove">Remove</button>');
-    $tr.append($tdButton);
-    $('#table').on('click', '.remove', function() {
-      $(this).closest("tr").remove();
-    })
-    $('#table').append($tr);
-  }
-}());
+	var stock;
+	this.stocks = [];
+	stock = new Stock();
+	this.stocks.push(stock);
+	createTableData(stock);
+			
+	function Stock() {	
+	  this.companyName = name;
+	  this.purchPrice = price;
+	  this.quantity = $quantity;
+	  this.dayChange = dayChange;
+	  this.percentChange = percentChange;
+	  this.id = idCounter++;
+	}
 
+	function createTableData(){
+	  var $tr = $('<tr>"stock.id"</tr>');
+	  var $tdName = $('<td>' + stock.companyName + '</td>');
+	  $tr.append($tdName);
+	  var $tdPrice = $('<td class="price">' + stock.purchPrice + '</td>');
+	  $tr.append($tdPrice);
+	  var $tdQuantity = $('<td>' + stock.quantity + '</td>');
+	  $tr.append($tdQuantity);
+	  var $tdChange = $('<td>' + stock.dayChange + '</td>');
+	  $tr.append($tdChange);
+	  var $tdChangePercent = $('<td>' + stock.percentChange + '</td>');
+	  $tr.append($tdChangePercent);
+	  var $tdButton = $('<button class="remove">Remove</button>');
+	  $tr.append($tdButton);
+	  $('#table').on('click', '.remove', function() {
+	    $(this).closest("tr").remove();
+	  })
+	  $('#table').append($tr);
+		
+    var total = 0;
+    $(".price").each(function() {
+			var value = $(this).text();
+  		if(!isNaN(value) && value.length != 0) {
+      total += parseFloat(value);
+  		}
+  	$("p").text("Total: $" + total.toFixed(2));
+		});
 
+		// Need event listener here to recalcuate the total if rows are removed...
+	}	
+}
